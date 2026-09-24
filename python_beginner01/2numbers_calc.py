253d830b86dafd09f2e6751d917807b94fedbb20
John Michael Tolentino <jmtolentino@John-Michael-Tolentinos-MacBook.local>
2026-09-24 22:50:52 +0800

    simple calc

---
+++ python_beginner01/2numbers_calc.py
@@ -0,0 +1,47 @@
+# A very simple calculator that only calculates 2 numbers.
+# This practice mainly exercises my fundamentals in functions and a bit of match-case statements.
+
+def numbers():
+    num1=int(input("Enter first number: "))
+    num2=int(input("Enter second number: "))
+    return num1, num2
+
+def add():
+    num1,num2=numbers()
+    sum=num1+num2
+    print(f'Sum: {sum}')
+
+def sub():
+    num1,num2=numbers()
+    diff=num1-num2
+    print(f'Difference: {diff}')
+
+def multiply():
+    num1,num2=numbers()
+    prod=num1*num2
+    print(f'Product: {prod}')
+
+def divide():
+    num1,num2=numbers()
+    quo=num1/num2
+    print(f'Quotient: {quo}')
+
+def function_match():
+    operation=input("Enter an arithmetic operation (+, -, *, /): ")
+    match operation:
+        case "+":
+            add()
+        case "-":
+            sub()
+        case "*":
+            multiply()
+        case "/":
+            divide()
+
+while True:
+    function_match()
+    operation_again=input("Would you like to do another calculation? (y/n): ")
+
+    if operation_again=="n":
+        print("Exiting calculator...")
+        break
